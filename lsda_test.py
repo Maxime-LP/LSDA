bef00a2382183876289df9128e42743d2573468e
amait41 <adrien.maitammar@gmail.com>
2021-09-29 19:32:04 +0200

    test

--- lsda_test.py
+++ lsda_test.py
@@ -1,71 +1,71 @@
-#X = data = 
-from matplotlib.colors import Colormap
 import numpy as np
 from sklearn.neighbors import kneighbors_graph
 from scipy.linalg import eig
 import matplotlib.pyplot as plt
 
-X = np.array([
-    [1,2,3],
-    [1,4,1],
-    [2,4,7],
-    [2,5,1],
-    [0,4,6],
-    [-1,50,12],
-    [20,30,15]
-])
-Y = [1,1,0,1,0,0,1]
 
-def get_W(X,K):
-    A = kneighbors_graph(X,K).toarray()
-    tmp = A+A.T
+def get_W(X, Y, K):
+    A = kneighbors_graph(X, K).toarray()
+    tmp = A + A.T
     tmp = np.array(tmp)
-    return np.ceil((A+A.T)/2)
+    W = np.ceil((A + A.T) / 2)
 
-def Wwb(X,Y,K):
-    W = get_W(X,K)
     n = np.shape(W)[0]
-    Ww = np.zeros((n,n))
-    Wb = np.zeros((n,n))
-
-    for i in range(0,n):
-        for j in range(0,n):
-            if W[i,j]==1:
-                if Y[i]==Y[j]:
-                    Ww[i,j]=1
+    Ww = np.zeros((n, n))
+    Wb = np.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            if W[i, j] == 1:
+                if Y[i] == Y[j]:
+                    Ww[i, j] = 1
                 else:
-                    Wb[i,j]=1
-    return Ww,Wb 
+                    Wb[i, j] = 1
+    return W, Ww, Wb
 
-def fit(X,Y,K,alpha,dim):
-    Ww,Wb = Wwb(X,Y,K)
-    Dw = np.diag(np.sum(Ww,axis=1))
-    Db = np.diag(np.sum(Wb,axis=1))
+
+def fit(X, Y, K, alpha, d):
+    W, Ww, Wb = get_W(X, Y, K)
+    Dw = np.diag(np.sum(Ww, axis=1))
+    Db = np.diag(np.sum(Wb, axis=1))
     Lb = Db - Wb
 
-    #print(X,'\n',Lb,'\n',Ww,'\n')
+    # Solves general eigenvalues/eigenvectors problem
+    left_mat = X.T.dot(alpha * Lb + (1 - alpha) * Ww).dot(X)  # nxn
+    right_mat = X.T.dot(Dw).dot(X)  # nxn
+    eigen_values, eigen_vectors = eig(left_mat, right_mat)
 
-    M1 = X.T.dot(alpha*Lb + (1-alpha)*Ww).dot(X) #nxn
-    M2 = X.T.dot(Dw).dot(X) #nxn
-    eigenValues,eigenVectors = eig(M1,M2)
-    idx = eigenValues.argsort()[::-1]   
-    eigenValues = eigenValues[idx]
-    eigenVectors = eigenVectors[:,idx]
-    #print(eigenValues,'\n',eigenVectors)
-    return X.dot(eigenVectors)[:,:dim]
+    # Sorts the eigenvectors according to their eigenvalues
+    idx = eigen_values.argsort()[::-1]
+    eigen_values = eigen_values[idx]
+    eigen_vectors = eigen_vectors[:, idx]
+
+    # Projects X on a subspace
+    projected_X = X.dot(eigen_vectors)[:, :d]
+
+    return projected_X
 
 
+X = np.array([
+    [1, 2, 3],
+    [2, 4, 7],
+    [1, 4, 1],
+    [2, 5, 1],
+    [0, 4, 6],
+    [-1, 50, 12],
+    [20, 30, 15]
+])
 
+Y = [1, 1, 0, 1, 0, 0, 1]
 
-Z = fit(X,Y,2,0.5,2)
+Z = fit(X, Y, 2, 0.5, 2)
 cdict = {0: 'red', 1: 'blue'}
 
-ax1 = plt.subplot(1,2,2,title = 'Après transformation Z = XA')
-ax2 = plt.subplot(1,2,1, title = 'Avant transformation')
+ax1 = plt.subplot(1, 2, 2, title='Après transformation Z = XA')
+ax2 = plt.subplot(1, 2, 1, title='Avant transformation')
 
 for g in np.unique(Y):
     ix = np.where(Y == g)
-    ax1.scatter(Z[ix,0], Z[ix,1], c = cdict[g], label = g)
-    ax2.scatter(X[ix,0], X[ix,1], c = cdict[g], alpha=0.6, label = g)
+    ax1.scatter(Z[ix, 0], Z[ix, 1], c=cdict[g], label=g)
+    ax2.scatter(X[ix, 0], X[ix, 1], c=cdict[g], alpha=0.6, label=g)
 ax2.legend()
 plt.show()
